05abe6becb9c4c9fda8350536f55da92abdade98
Littlears <74994475+vanilla015@users.noreply.github.com>
2021-09-06 00:27:20 +0200

    Add files via upload

---
+++ Nigga Gen/Proxy tool12/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ButtonReset = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button
+        '
+        Me.Button.Location = New System.Drawing.Point(12, 12)
+        Me.Button.Name = "Button"
+        Me.Button.Size = New System.Drawing.Size(233, 79)
+        Me.Button.TabIndex = 0
+        Me.Button.Text = "Start"
+        Me.Button.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(251, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(271, 426)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(12, 117)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Total:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(44, 117)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "0"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(12, 91)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(233, 23)
+        Me.NumericUpDown1.TabIndex = 4
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 135)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(233, 23)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'ButtonReset
+        '
+        Me.ButtonReset.Location = New System.Drawing.Point(12, 415)
+        Me.ButtonReset.Name = "ButtonReset"
+        Me.ButtonReset.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonReset.TabIndex = 6
+        Me.ButtonReset.Text = "Reset"
+        Me.ButtonReset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Button
+        Me.ClientSize = New System.Drawing.Size(534, 450)
+        Me.Controls.Add(Me.ButtonReset)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Code Gen By A. n't"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ButtonReset As Button
+End Class
---
+++ Nigga Gen/Proxy tool12/Form1.vb
@@ -0,0 +1,51 @@
+﻿
+
+
+Public Class Form1
+    Private Sub FormImagenFondoUno_Load(
+            ByVal sender As System.Object,
+            ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Call EstablecerImagenFondoParaFormLoad()
+    End Sub
+    Private Sub EstablecerImagenFondoParaFormLoad()
+        Try
+            Me.BackgroundImage =
+        Image.FromFile(".\img\Imagen1.jpg")
+            Me.BackgroundImageLayout = ImageLayout.Stretch
+
+        Finally
+        End Try
+    End Sub
+    Private Sub FormImagenFondoUno_Resize(
+        ByVal sender As Object, ByVal e As System.EventArgs) _
+         Handles Me.Resize
+        Me.Refresh()
+    End Sub
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button.Click
+        ProgressBar1.Maximum = NumericUpDown1.Value
+        Do Until Label1.Text = NumericUpDown1.Value
+            Dim caracteres As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            Dim num As Integer = 16
+            Dim RND As New Random
+            Dim code As String = ""
+            Do Until code.Length = num
+                code += caracteres.ToCharArray(RND.Next(0, caracteres.Length), 1)
+            Loop
+            ProgressBar1.Increment(1)
+            Label1.Text += 1
+            TextBox1.Text =
+        TextBox1.Text & vbNewLine & "https://discord.gift/" & code
+        Loop
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        NumericUpDown1.Maximum = Decimal.MaxValue
+    End Sub
+
+    Private Sub ButtonReset_Click(sender As Object, e As EventArgs) Handles ButtonReset.Click
+        TextBox1.Text = ""
+        Label1.Text = 0
+        ProgressBar1.Value = 0
+    End Sub
+End Class
