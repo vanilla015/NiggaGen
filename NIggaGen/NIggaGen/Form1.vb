abbc470266b8c59e9e032065356c87b50d04abcf
Littlears <74994475+vanilla015@users.noreply.github.com>
2021-09-12 06:32:31 +0200

    Add files via upload

---
+++ NIggaGen/NIggaGen/Form1.vb
@@ -0,0 +1,64 @@
+﻿
+
+
+Public Class Form1
+    Private Sub FormImagenFondoUno_Load(
+            ByVal sender As System.Object,
+            ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Call EstablecerImagenFondoParaFormLoad()
+    End Sub
+    Private Sub EstablecerImagenFondoParaFormLoad()
+        Try
+            Me.BackgroundImage =
+        Image.FromFile(".\img\Imagen1.jpg")
+            Me.BackgroundImageLayout = ImageLayout.Stretch
+
+        Finally
+        End Try
+    End Sub
+    Private Sub FormImagenFondoUno_Resize(
+        ByVal sender As Object, ByVal e As System.EventArgs) _
+         Handles Me.Resize
+        Me.Refresh()
+    End Sub
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button.Click
+        ProgressBar1.Maximum = NumericUpDown1.Value
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        NumericUpDown1.Maximum = Decimal.MaxValue
+    End Sub
+
+    Private Sub ButtonReset_Click(sender As Object, e As EventArgs) Handles ButtonReset.Click
+        TextBox1.Text = ""
+        Label1.Text = 0
+        ProgressBar1.Value = 0
+    End Sub
+
+    Public Sub New()
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+        Control.CheckForIllegalCrossThreadCalls = False
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Do Until Label1.Text = NumericUpDown1.Value
+            Dim caracteres As String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            Dim num As Integer = 16
+            Dim RND As New Random
+            Dim code As String = ""
+            Do Until code.Length = num
+                code += caracteres.ToCharArray(RND.Next(0, caracteres.Length), 1)
+            Loop
+            ProgressBar1.Increment(1)
+            Label1.Text += 1
+            TextBox1.Text =
+        TextBox1.Text & vbNewLine & "https://discord.gift/" & code
+        Loop
+    End Sub
+End Class
